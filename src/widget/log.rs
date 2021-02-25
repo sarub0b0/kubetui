b8c7da326dc859ce622f45434a23271fab3c802f
sarub0b0 <ekr59uv25@gmail.com>
2021-02-25 20:06:26 +0900

    feat(log): カラースタイルを適用する関数を実装

--- src/widget/log.rs
+++ src/widget/log.rs
@@ -1,8 +1,8 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use tui::style::Style;
-use tui::text::Spans;
+use tui::style::{Color, Style};
+use tui::text::{Span, Spans, Text};
 use tui::widgets::{Block, Paragraph, Wrap};
 
 use super::WidgetTrait;
@@ -92,6 +92,7 @@ impl<'a> Logs<'a> {
         self.items = items.clone();
 
         self.spans = items.iter().cloned().map(Spans::from).collect();
+
         self.paragraph = Paragraph::new(self.spans.clone())
             .style(Style::default())
             .wrap(Wrap { trim: false });
@@ -125,4 +126,96 @@ impl WidgetTrait for Logs<'_> {
     fn selectable(&self) -> bool {
         true
     }
+}
+
+fn style(num: &str) -> Style {
+    let color = match num {
+        "30" => Color::Black,
+        "31" => Color::Red,
+        "32" => Color::Green,
+        "33" => Color::Yellow,
+        "34" => Color::Blue,
+        "35" => Color::Magenta,
+        "36" => Color::Cyan,
+        "37" => Color::White,
+        "39" => Color::Reset,
+        "90" => Color::DarkGray,
+        "91" => Color::LightRed,
+        "92" => Color::LightGreen,
+        "93" => Color::LightYellow,
+        "94" => Color::LightBlue,
+        "95" => Color::LightMagenta,
+        "96" => Color::LightCyan,
+        "97" => Color::Gray,
+        _ => Color::Reset,
+    };
+
+    Style::default().fg(color)
+}
+
+fn generate_spans(text: &Vec<std::string::String>) -> Vec<Spans> {
+    let mut ret: Vec<Spans> = Vec::with_capacity(1024);
+
+    for t in text {
+        // println!("{} {:#?}\n", t.len(), t);
+
+        let buf = &t[0..];
+        let mut start = 0;
+        let mut end = 0;
+        let mut finded = false;
+
+        let mut spans: Vec<Span> = vec![];
+        while let Some(i) = buf[start..].find("\x1b[") {
+            finded = true;
+            start = i + 5 + end;
+
+            let (c0, c1) = (i + 2 + end, i + 4 + end);
+
+            if let Some(next) = buf[start..].find("\x1b[") {
+                end = next + start;
+            } else {
+                end = t.len();
+            }
+            // println!("i:{} range:{}-{} {}", i, start, end, &buf[start..]);
+            spans.push(Span::styled(&buf[start..end], style(&buf[c0..c1])));
+
+            start = end;
+        }
+
+        if finded == false {
+            ret.push(Spans::from(t.clone()));
+        } else {
+            ret.push(Spans::from(spans));
+        }
+    }
+
+    // println!("{:#?}", ret);
+
+    ret
+}
+
+#[test]
+fn parse_ansi_escape_colors() {
+    let text = vec![
+            "> taskbox@0.1.0 start /app",
+            "> react-scripts start",
+            "",
+            "\x1b[34mℹ\x1b[39m \x1b[90m｢wds｣\x1b[39m: Project is running at http://10.1.157.32/",
+            "\x1b[34mℹ\x1b[39m \x1b[90m｢wds｣\x1b[39m: webpack output is served from",
+            "\x1b[34mℹ\x1b[39m \x1b[90m｢wds｣\x1b[39m: Content not from webpack is served from /app/public",
+            "\x1b[34mℹ\x1b[39m \x1b[90m｢wds｣\x1b[39m: 404s will fallback to /",
+            "Starting the development server...",
+            "",
+            "Compiled successfully!",
+            "",
+            "You can now view taskbox in the browser.",
+            "",
+            "  Local:            http://localhost:3000",
+            "  On Your Network:  http://10.1.157.32:3000",
+            "",
+            "Note that the development build is not optimized.",
+            "To create a production build, use npm run build.",
+        ];
+
+    println!("{:?}", text);
 }
