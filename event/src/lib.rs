40da79107e24b3962808a4c23e8424dcfe7b1674
sarub0b0 <ekr59uv25@gmail.com>
2021-05-21 13:49:16 +0900

    feat(event): Mouseイベントを渡すように変更

--- event/src/lib.rs
+++ event/src/lib.rs
@@ -6,12 +6,12 @@ pub mod kubernetes;
 mod util;
 
 use crate::kubernetes::Kube;
-use crossterm::event::KeyEvent;
+use crossterm::event::{KeyEvent, MouseEvent};
 
 pub enum Event {
     Input(KeyEvent),
     Kube(Kube),
     Tick,
     Resize(u16, u16),
-    Mouse,
+    Mouse(MouseEvent),
 }
