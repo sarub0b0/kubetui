ea417ad27516bb39291f9d47e52af14ba2b502ce
sarub0b0 <ekr59uv25@gmail.com>
2022-02-16 00:47:21 +0900

    chore(network/description/pod): pod情報のキーを小文字に変更

--- src/event/kubernetes/network/description/pod/fetched_pod.rs
+++ src/event/kubernetes/network/description/pod/fetched_pod.rs
@@ -7,7 +7,7 @@ pub struct FetchedPod(pub Pod);
 
 impl FetchedPod {
     pub fn to_string_vec(&self) -> Vec<String> {
-        let mut ret = vec!["Pod:".to_string()];
+        let mut ret = vec!["pod:".to_string()];
 
         if let Some(labels) = &self.0.metadata.labels {
             let labels = labels
@@ -15,7 +15,7 @@ impl FetchedPod {
                 .map(|(k, v)| format!("    {}: {}", k, v))
                 .collect::<Vec<String>>();
 
-            ret.push("  Labels:".to_string());
+            ret.push("  labels:".to_string());
 
             ret.extend(labels);
         }
@@ -32,18 +32,18 @@ impl FetchedPod {
                 .collect::<Vec<String>>();
 
             if status.host_ip.is_some() || status.pod_ip.is_some() || !pod_ips.is_empty() {
-                ret.push("  IP:".to_string());
+                ret.push("  ip:".to_string());
 
                 if let Some(host_ip) = &status.host_ip {
-                    ret.push(format!("    HostIP: {}", host_ip));
+                    ret.push(format!("    hostIP: {}", host_ip));
                 }
 
                 if let Some(pod_ip) = &status.pod_ip {
-                    ret.push(format!("    PodIP: {}", pod_ip));
+                    ret.push(format!("    podIP: {}", pod_ip));
                 }
 
                 if !pod_ips.is_empty() {
-                    ret.push("    PodIPs:".to_string());
+                    ret.push("    podIPs:".to_string());
 
                     ret.extend(pod_ips);
                 }
@@ -51,38 +51,38 @@ impl FetchedPod {
         }
 
         if let Some(spec) = &self.0.spec {
-            ret.push("  Containers:".to_string());
+            ret.push("  containers:".to_string());
 
             let containers: Vec<String> = spec
                 .containers
                 .iter()
                 .flat_map(|c| {
-                    let mut vec = vec![format!("    - Name: {}", c.name)];
+                    let mut vec = vec![format!("    - name: {}", c.name)];
 
                     if let Some(image) = &c.image {
-                        vec.push(format!("      Image: {}", image));
+                        vec.push(format!("      image: {}", image));
                     }
 
                     if let Some(ports) = &c.ports {
-                        vec.push("      Ports:".to_string());
+                        vec.push("      ports:".to_string());
 
                         ports.iter().for_each(|port| {
-                            vec.push(format!("        ContainerPort: {}", port.container_port));
+                            vec.push(format!("        containerPort: {}", port.container_port));
 
                             if let Some(host_ip) = &port.host_ip {
-                                vec.push(format!("        HostIP: {}", host_ip));
+                                vec.push(format!("        hostIP: {}", host_ip));
                             }
 
                             if let Some(host_port) = &port.host_port {
-                                vec.push(format!("        HostPort: {}", host_port));
+                                vec.push(format!("        hostPort: {}", host_port));
                             }
 
                             if let Some(name) = &port.name {
-                                vec.push(format!("        Name: {}", name));
+                                vec.push(format!("        name: {}", name));
                             }
 
                             if let Some(protocol) = &port.protocol {
-                                vec.push(format!("        Protocol: {}", protocol));
+                                vec.push(format!("        protocol: {}", protocol));
                             }
                         })
                     }
