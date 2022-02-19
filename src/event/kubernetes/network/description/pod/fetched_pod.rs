83308646c606842643494757b089b7750c5513bb
sarub0b0 <ekr59uv25@gmail.com>
2022-02-19 15:30:31 +0900

    refactor(network): fetched_podをリファクタリング

--- src/event/kubernetes/network/description/pod/fetched_pod.rs
+++ src/event/kubernetes/network/description/pod/fetched_pod.rs
@@ -1,4 +1,7 @@
-use k8s_openapi::api::core::v1::Pod;
+use k8s_openapi::{
+    api::core::v1::{Pod, PodSpec, PodStatus},
+    apimachinery::pkg::apis::meta::v1::ObjectMeta,
+};
 
 use super::*;
 
@@ -9,20 +12,44 @@ impl FetchedPod {
     pub fn to_vec_string(&self) -> Vec<String> {
         let mut ret = vec!["pod:".to_string()];
 
-        if let Some(labels) = &self.0.metadata.labels {
+        if let Some(value) = Self::metadata(&self.0.metadata) {
+            ret.extend(value);
+        }
+
+        if let Some(value) = Self::spec(&self.0.spec) {
+            ret.extend(value);
+        }
+
+        if let Some(value) = Self::status(&self.0.status) {
+            ret.extend(value)
+        }
+
+        ret
+    }
+
+    fn metadata(metadata: &ObjectMeta) -> Option<Vec<String>> {
+        if let Some(labels) = &metadata.labels {
             let labels = labels
                 .iter()
                 .map(|(k, v)| format!("    {}: {}", k, v))
                 .collect::<Vec<String>>();
 
-            ret.push("  labels:".to_string());
+            if !labels.is_empty() {
+                let mut ret = vec!["  labels:".to_string()];
 
-            ret.extend(labels);
+                ret.extend(labels);
+
+                Some(ret)
+            } else {
+                None
+            }
+        } else {
+            None
         }
+    }
 
-        if let Some(spec) = &self.0.spec {
-            ret.push("  containers:".to_string());
-
+    fn spec(spec: &Option<PodSpec>) -> Option<Vec<String>> {
+        if let Some(spec) = spec {
             let containers: Vec<String> = spec
                 .containers
                 .iter()
@@ -33,8 +60,7 @@ impl FetchedPod {
                         vec.push(format!("      image: {}", image));
                     }
 
-                    // そのままserde_yaml::to_stringにいれると"~"になるため中身を取り出す処理をいれ
-                    // ている
+                    // そのままserde_yaml::to_stringにいれると"~"になるため中身を取り出す処理をいれている
                     if let Some(ports) = &c.ports {
                         if let Ok(ports) = serde_yaml::to_string(ports) {
                             let v = ports
@@ -54,10 +80,23 @@ impl FetchedPod {
                 })
                 .collect();
 
-            ret.extend(containers);
-        }
+            if !containers.is_empty() {
+                let mut ret = vec!["  containers:".to_string()];
+                ret.extend(containers);
 
-        if let Some(status) = &self.0.status {
+                Some(ret)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    fn status(status: &Option<PodStatus>) -> Option<Vec<String>> {
+        if let Some(status) = status {
+            let mut ret = Vec::new();
+
             if let Some(host_ip) = &status.host_ip {
                 ret.push(format!("  hostIP: {}", host_ip));
             }
@@ -82,9 +121,15 @@ impl FetchedPod {
             if let Some(phase) = &status.phase {
                 ret.push(format!("  phase: {}", phase));
             }
-        }
 
-        ret
+            if !ret.is_empty() {
+                Some(ret)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
     }
 }
 
