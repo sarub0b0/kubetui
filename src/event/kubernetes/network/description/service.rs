0ad5ebe759a124c8eaa6ff07b4c0593a81b2d30f
sarub0b0 <ekr59uv25@gmail.com>
2022-02-20 21:46:08 +0900

    feat(network): descriptionにserviceのyamlを表示する機能を仮実装

--- src/event/kubernetes/network/description/service.rs
+++ src/event/kubernetes/network/description/service.rs
@@ -1,8 +1,12 @@
 use crossbeam::channel::Sender;
+use k8s_openapi::api::core::v1::Service;
 
 use crate::{
     error::Result,
-    event::{kubernetes::client::KubeClient, Event},
+    event::{
+        kubernetes::{client::KubeClient, network::NetworkMessage},
+        Event,
+    },
 };
 
 use super::DescriptionWorker;
@@ -12,16 +16,37 @@ pub(super) struct ServiceDescriptionWorker<'a> {
     tx: &'a Sender<Event>,
     namespace: String,
     name: String,
-    url: String,
 }
 
 #[async_trait::async_trait]
 impl<'a> DescriptionWorker<'a> for ServiceDescriptionWorker<'a> {
-    fn new(_: &'a KubeClient, _: &'a Sender<Event>, _: String, _: String) -> Self {
-        todo!()
+    fn new(client: &'a KubeClient, tx: &'a Sender<Event>, namespace: String, name: String) -> Self {
+        Self {
+            client,
+            tx,
+            namespace,
+            name,
+        }
     }
 
     async fn run(&self) -> Result<()> {
-        todo!()
+        let url = format!(
+            "api/v1/namespaces/{}/services/{}",
+            self.namespace, self.name
+        );
+
+        let res = self.client.request_text(&url).await?;
+
+        let value: Service = serde_json::from_str(&res)?;
+
+        let value = serde_yaml::to_string(&value)?
+            .lines()
+            .skip(1)
+            .map(ToString::to_string)
+            .collect();
+
+        self.tx.send(NetworkMessage::Response(Ok(value)).into())?;
+
+        Ok(())
     }
 }
