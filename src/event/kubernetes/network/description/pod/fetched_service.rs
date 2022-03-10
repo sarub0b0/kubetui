f18772173fb9136e75cf2a00394360cc4977955d
kosay <ekr59uv25@gmail.com>
2022-03-11 01:02:39 +0900

    test(network/pod): fetched_serviceのテスト実装

--- src/event/kubernetes/network/description/pod/fetched_service.rs
+++ src/event/kubernetes/network/description/pod/fetched_service.rs
@@ -26,4 +26,41 @@ impl FetchedService {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn 複数のserviceを持つときリソース名を含むvalueを返す() {
+        let actual = FetchedService(vec![
+            Service {
+                metadata: ObjectMeta {
+                    name: Some("foo".into()),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            Service {
+                metadata: ObjectMeta {
+                    name: Some("bar".into()),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        ])
+        .to_value();
+
+        let expected: Vec<Value> = vec!["foo".into(), "bar".into()];
+
+        assert_eq!(actual, Some(expected.into()));
+    }
+
+    #[test]
+    fn serviceを持たないときnoneを返す() {
+        let actual = FetchedService(vec![]).to_value();
+
+        let expected = None;
+
+        assert_eq!(actual, expected);
+    }
 }
