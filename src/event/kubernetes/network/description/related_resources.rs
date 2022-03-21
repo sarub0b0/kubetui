c3089f7415a401220e9fd7de53f39879e3776154
kosay <ekr59uv25@gmail.com>
2022-03-22 03:01:40 +0900

    refactor(related_resources/pod): related_resourcesのテスト実装のためfetch関数を専用の構造体に分離

--- src/event/kubernetes/network/description/related_resources.rs
+++ src/event/kubernetes/network/description/related_resources.rs
@@ -155,7 +155,86 @@ mod pod {
             }
         }
 
-        mod fetch {
+        mod filter {
+            use super::*;
+
+            #[ignore]
+            #[test]
+            fn 関連するpodのリストを生成する() {}
+
+            #[ignore]
+            #[test]
+            fn 関連するpodがないときnoneを返す() {}
+        }
+
+        mod to_value {
+            use super::*;
+
+            #[ignore]
+            #[test]
+            fn podのリストからnameのリストをvalue型で返す() {}
+
+            #[ignore]
+            #[test]
+            fn リストが空のときnoneを返す() {}
+        }
+    }
+
+    mod fetch {
+        use crate::event::kubernetes::client::KubeClientRequest;
+
+        use anyhow::Result;
+
+        use super::FetchedPodList;
+
+        pub struct FetchPodClient<'a, C: KubeClientRequest> {
+            client: &'a C,
+            namespace: &'a str,
+        }
+
+        impl<'a, C: KubeClientRequest> FetchPodClient<'a, C> {
+            pub fn new(client: &'a C, namespace: &'a str) -> Self {
+                Self { client, namespace }
+            }
+        }
+
+        #[async_trait::async_trait]
+        pub trait Fetch {
+            async fn fetch(&self) -> Result<FetchedPodList>;
+        }
+
+        #[async_trait::async_trait]
+        impl<'a, C: KubeClientRequest> Fetch for FetchPodClient<'_, C> {
+            async fn fetch(&self) -> Result<FetchedPodList> {
+                let url = format!("api/v1/namespaces/{}/pods", self.namespace);
+
+                self.client.request(&url).await
+            }
+        }
+
+        #[cfg(test)]
+        pub mod mock {
+            use super::{Fetch, FetchedPodList, KubeClientRequest, Result};
+
+            use mockall::mock;
+
+            mock! {
+                pub TestFetchPodClient {}
+
+                impl Clone for TestFetchPodClient {
+                    fn clone(&self) -> Self ;
+                }
+
+                #[async_trait::async_trait]
+                impl Fetch for TestFetchPodClient {
+                    async fn fetch(&self) -> Result<FetchedPodList> ;
+                }
+            }
+        }
+
+        #[cfg(test)]
+        mod tests {
+
             use indoc::indoc;
             use mockall::predicate::eq;
 
@@ -211,9 +290,7 @@ mod pod {
                     Ok(pod_one())
                 );
 
-                let selector = BTreeMap::from([("app", "test"), ("version", "v1")]);
-
-                let client = FetchPodClient::new(&client, "default", selector);
+                let client = FetchPodClient::new(&client, "default");
 
                 let result = client.fetch().await;
 
@@ -232,38 +309,12 @@ mod pod {
                     bail!("error")
                 );
 
-                let selector = BTreeMap::from([("app", "test"), ("version", "v1")]);
-
-                let client = FetchPodClient::new(&client, "default", selector);
+                let client = FetchPodClient::new(&client, "default");
 
                 let result = client.fetch().await;
 
                 assert_eq!(result.is_err(), true);
             }
-        }
-
-        mod filter {
-            use super::*;
-
-            #[ignore]
-            #[test]
-            fn 関連するpodのリストを生成する() {}
-
-            #[ignore]
-            #[test]
-            fn 関連するpodがないときnoneを返す() {}
-        }
-
-        mod to_value {
-            use super::*;
-
-            #[ignore]
-            #[test]
-            fn podのリストからnameのリストをvalue型で返す() {}
-
-            #[ignore]
-            #[test]
-            fn リストが空のときnoneを返す() {}
         }
     }
 }
