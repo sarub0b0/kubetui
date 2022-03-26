f597195680c940217c7e89b08f603ad4ed2286cd
kosay <ekr59uv25@gmail.com>
2022-03-26 23:29:13 +0900

    feat(related_resources): データを取得するfetchモジュールを抽象化

--- src/event/kubernetes/network/description/related_resources.rs
+++ src/event/kubernetes/network/description/related_resources.rs
@@ -62,6 +62,152 @@ mod btree_map_contains_key_values {
     }
 }
 
+mod fetch {
+    use std::marker::PhantomData;
+
+    use k8s_openapi::{List, ListableResource};
+    use kube::Resource;
+    use serde::de::DeserializeOwned;
+
+    use super::*;
+
+    use crate::event::kubernetes::client::KubeClientRequest;
+
+    pub struct FetchClient<'a, C: KubeClientRequest> {
+        client: &'a C,
+        namespace: &'a str,
+    }
+
+    impl<'a, C: KubeClientRequest> FetchClient<'a, C> {
+        pub fn new(client: &'a C, namespace: &'a str) -> Self {
+            Self { client, namespace }
+        }
+    }
+
+    impl<'a, C: KubeClientRequest> FetchClient<'_, C> {
+        pub async fn fetch<K>(&self) -> Result<List<K>>
+        where
+            K: Resource<DynamicType = ()> + ListableResource,
+            K: DeserializeOwned + 'static,
+        {
+            let url = K::url_path(&(), Some(self.namespace));
+
+            self.client.request(&url).await
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+
+        use indoc::indoc;
+        use k8s_openapi::api::core::v1::{Pod, Service};
+        use mockall::predicate::eq;
+
+        use crate::{event::kubernetes::client::mock::MockTestKubeClient, mock_expect};
+
+        use anyhow::bail;
+
+        use super::*;
+
+        fn pod() -> List<Pod> {
+            let yaml = indoc! {
+            "
+            items:
+              - metadata:
+                name: pod-1
+                labels:
+                  app: pod-1
+                  version: v1
+              - metadata:
+                name: pod-2
+                labels:
+                  app: pod-2
+                  version: v1
+            "
+            };
+
+            serde_yaml::from_str(&yaml).unwrap()
+        }
+
+        fn service() -> List<Service> {
+            let yaml = indoc! {
+            "
+            items:
+              - metadata:
+                name: service-1
+                labels:
+                  app: service-1
+                  version: v1
+              - metadata:
+                name: service-2
+                labels:
+                  app: service-2
+                  version: v1
+            "
+            };
+
+            serde_yaml::from_str(&yaml).unwrap()
+        }
+
+        #[tokio::test]
+        async fn podリストを取得する() {
+            let mut client = MockTestKubeClient::new();
+
+            mock_expect!(
+                client,
+                request,
+                List<Pod>,
+                eq("/api/v1/namespaces/default/pods"),
+                { Ok(pod()) }
+            );
+
+            let client = FetchClient::new(&client, "default");
+
+            let result: Result<List<Pod>> = client.fetch().await;
+
+            assert_eq!(result.is_ok(), true);
+        }
+
+        #[tokio::test]
+        async fn serviceリストを取得する() {
+            let mut client = MockTestKubeClient::new();
+
+            mock_expect!(
+                client,
+                request,
+                List<Service>,
+                eq("/api/v1/namespaces/default/services"),
+                { Ok(service()) }
+            );
+
+            let client = FetchClient::new(&client, "default");
+
+            let result: Result<List<Service>> = client.fetch().await;
+
+            assert_eq!(result.is_ok(), true);
+        }
+
+        #[tokio::test]
+        async fn エラーのときerrを返す() {
+            let mut client = MockTestKubeClient::new();
+
+            mock_expect!(
+                client,
+                request,
+                List<Pod>,
+                eq("/api/v1/namespaces/default/pods"),
+                bail!("error")
+            );
+
+            let client = FetchClient::new(&client, "default");
+
+            let result: Result<List<Pod>> = client.fetch().await;
+
+            assert_eq!(result.is_err(), true);
+        }
+    }
+}
+
 mod to_value {
 
     use k8s_openapi::{api::core::v1::Pod, List, ListableResource};
