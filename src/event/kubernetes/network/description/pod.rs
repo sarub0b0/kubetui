386a57236f9cede98ea39f170cfeadf710931264
kosay <ekr59uv25@gmail.com>
2022-03-27 19:16:21 +0900

    chore(description/pod): RelatedResources導入により不要なファイル、テストを削除

--- src/event/kubernetes/network/description/pod.rs
+++ src/event/kubernetes/network/description/pod.rs
@@ -1,12 +1,8 @@
-#![allow(dead_code)]
-mod fetched_ingress;
-mod fetched_network_policy;
 mod fetched_pod;
-mod fetched_service;
 
 pub(super) use fetched_pod::*;
 
-use k8s_openapi::api::{core::v1::Pod, networking::v1::IngressSpec};
+use k8s_openapi::api::core::v1::Pod;
 use kube::{Resource, ResourceExt};
 use serde_yaml::{Mapping, Value};
 
@@ -18,24 +14,9 @@ use super::{
     Fetch, FetchedData, Result,
 };
 
-use std::collections::BTreeMap;
-
 use serde::Deserialize;
 
 use crate::event::kubernetes::client::KubeClientRequest;
-
-#[derive(Debug)]
-enum FetchArgs {
-    Value(String),
-    List(Vec<String>),
-}
-
-trait FetchedResource {
-    type Resource;
-
-    fn fetch(args: Option<FetchArgs>) -> Result<Self::Resource>;
-    fn to_value(&self) -> Option<Value>;
-}
 
 pub(super) struct PodDescriptionWorker<'a, C>
 where
@@ -56,7 +37,6 @@ impl<'a, C: KubeClientRequest> Fetch<'a, C> for PodDescriptionWorker<'a, C> {
         }
     }
 
-    // TODO 関連するService, Ingress, NetworkPolicyの情報を合わせて表示する
     async fn fetch(&self) -> Result<FetchedData> {
         let mut value = Vec::new();
 
@@ -108,114 +88,27 @@ impl<'a, C: KubeClientRequest> Fetch<'a, C> for PodDescriptionWorker<'a, C> {
     }
 }
 
-fn contains_service_into_ingress(ingress_spec: &IngressSpec, services: &[String]) -> bool {
-    ingress_spec
-        .default_backend
-        .as_ref()
-        .map_or(false, |default_backend| {
-            default_backend
-                .service
-                .as_ref()
-                .map_or(false, |backend_service| {
-                    services
-                        .iter()
-                        .any(|service| &backend_service.name == service)
-                })
-        })
-        || ingress_spec.rules.as_ref().map_or(false, |rules| {
-            rules.iter().any(|rule| {
-                rule.http.as_ref().map_or(false, |http| {
-                    http.paths.iter().any(|path| {
-                        path.backend
-                            .service
-                            .as_ref()
-                            .map_or(false, |backend_service| {
-                                services
-                                    .iter()
-                                    .any(|service| &backend_service.name == service)
-                            })
-                    })
-                })
-            })
-        })
-}
-
-fn contains_key_values(
-    lhs: &Option<BTreeMap<String, String>>,
-    rhs: &Option<BTreeMap<String, String>>,
-) -> bool {
-    #[cfg(feature = "logging")]
-    ::log::debug!("match_selector {:#?} <=> {:#?}", lhs, rhs);
-
-    lhs.as_ref().map_or(false, |lhs| {
-        rhs.as_ref().map_or(false, |rhs| {
-            lhs.iter().all(|(lhs_key, lhs_value)| {
-                rhs.get(lhs_key)
-                    .map_or(false, |rhs_value| lhs_value == rhs_value)
-            })
-        })
-    })
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    mod match_selector {
-        use super::*;
+    use crate::{event::kubernetes::client::mock::MockTestKubeClient, mock_expect};
+    use indoc::indoc;
+    use k8s_openapi::{
+        api::{
+            core::v1::{Pod, Service},
+            networking::v1::Ingress,
+        },
+        List,
+    };
+    use mockall::predicate::eq;
 
-        #[test]
-        fn lhsの値すべてがrhsにふくまれていればtrueを返す() {
-            let lhs = Some(BTreeMap::from_iter(vec![
-                ("a".to_string(), "aaa".to_string()),
-                ("b".to_string(), "bbb".to_string()),
-            ]));
+    use pretty_assertions::assert_eq;
 
-            let rhs = Some(BTreeMap::from_iter(vec![
-                ("a".to_string(), "aaa".to_string()),
-                ("b".to_string(), "bbb".to_string()),
-                ("c".to_string(), "ccc".to_string()),
-            ]));
+    use anyhow::anyhow;
 
-            assert!(contains_key_values(&lhs, &rhs));
-        }
-
-        #[test]
-        fn lhsの値すべてがrhsにふくまれていなければfalseを返す() {
-            let lhs = Some(BTreeMap::from_iter(vec![
-                ("a".to_string(), "aaa".to_string()),
-                ("b".to_string(), "bbb".to_string()),
-            ]));
-
-            let rhs = Some(BTreeMap::from_iter(vec![
-                ("b".to_string(), "bbb".to_string()),
-                ("c".to_string(), "ccc".to_string()),
-            ]));
-
-            assert!(!contains_key_values(&lhs, &rhs));
-        }
-    }
-
-    mod fetch {
-        use super::*;
-
-        use crate::{event::kubernetes::client::mock::MockTestKubeClient, mock_expect};
-        use indoc::indoc;
-        use k8s_openapi::{
-            api::{
-                core::v1::{Pod, Service},
-                networking::v1::Ingress,
-            },
-            List,
-        };
-        use mockall::predicate::eq;
-
-        use pretty_assertions::assert_eq;
-
-        use anyhow::anyhow;
-
-        fn setup_pod() -> FetchedPod {
-            let yaml = indoc! {
+    fn setup_pod() -> FetchedPod {
+        let yaml = indoc! {
             "
             metadata:
               name: test
@@ -233,15 +126,16 @@ mod tests {
               podIP: 10.1.0.21
               podIPs:
                 - ip: 10.1.0.21
-            " };
+            "
+        };
 
-            let pod: Pod = serde_yaml::from_str(&yaml).unwrap();
+        let pod: Pod = serde_yaml::from_str(&yaml).unwrap();
 
-            FetchedPod(pod)
-        }
+        FetchedPod(pod)
+    }
 
-        fn setup_services() -> List<Service> {
-            let yaml = indoc! {
+    fn setup_services() -> List<Service> {
+        let yaml = indoc! {
             "
             items:
               - metadata:
@@ -255,13 +149,13 @@ mod tests {
                   selector:
                     job-name: kubetui-text-color
             "
-            };
+        };
 
-            serde_yaml::from_str(&yaml).unwrap()
-        }
+        serde_yaml::from_str(&yaml).unwrap()
+    }
 
-        fn setup_ingresses() -> List<Ingress> {
-            let yaml = indoc! {
+    fn setup_ingresses() -> List<Ingress> {
+        let yaml = indoc! {
             "
             items:
               - metadata:
@@ -283,16 +177,16 @@ mod tests {
                               service:
                                 name: service-2
             "
-            };
+        };
 
-            serde_yaml::from_str(&yaml).unwrap()
-        }
+        serde_yaml::from_str(&yaml).unwrap()
+    }
 
-        #[tokio::test(flavor = "multi_thread")]
-        async fn yamlデータを送信してokを返す() {
-            let mut client = MockTestKubeClient::new();
-            mock_expect!(
-                client,
+    #[tokio::test(flavor = "multi_thread")]
+    async fn yamlデータを送信してokを返す() {
+        let mut client = MockTestKubeClient::new();
+        mock_expect!(
+            client,
                 request,
                 [
                     (
@@ -313,45 +207,44 @@ mod tests {
                 ]
             );
 
-            let worker =
-                PodDescriptionWorker::new(&client, "default".to_string(), "test".to_string());
+        let worker = PodDescriptionWorker::new(&client, "default".to_string(), "test".to_string());
 
-            let result = worker.fetch().await;
+        let result = worker.fetch().await;
 
-            let expected: Vec<String> = indoc! {
-                "
-                pod:
-                  labels:
-                    controller-uid: 30d417a8-cb1c-467b-92fe-7819601a6ef8
-                    job-name: kubetui-text-color
-                  containers:
-                    - name: job
-                      image: alpine
-                  hostIP: 192.168.65.4
-                  podIP: 10.1.0.21
-                  podIPs: 10.1.0.21
-                  phase: Succeeded
+        let expected: Vec<String> = indoc! {
+            "
+            pod:
+              labels:
+                controller-uid: 30d417a8-cb1c-467b-92fe-7819601a6ef8
+                job-name: kubetui-text-color
+              containers:
+                - name: job
+                  image: alpine
+              hostIP: 192.168.65.4
+              podIP: 10.1.0.21
+              podIPs: 10.1.0.21
+              phase: Succeeded
 
-                relatedResources:
-                  services:
-                    - service-1
-                    - service-2
-                  ingresses:
-                    - ingress-1
-                    - ingress-2
-                "
-            }
-            .lines()
-            .map(ToString::to_string)
-            .collect();
-
-            assert_eq!(result.unwrap(), expected)
+            relatedResources:
+              services:
+                - service-1
+                - service-2
+              ingresses:
+                - ingress-1
+                - ingress-2
+            "
         }
+        .lines()
+        .map(ToString::to_string)
+        .collect();
 
-        #[tokio::test(flavor = "multi_thread")]
-        async fn エラーが出たときerrを返す() {
-            let mut client = MockTestKubeClient::new();
-            mock_expect!(
+        assert_eq!(result.unwrap(), expected)
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn エラーが出たときerrを返す() {
+        let mut client = MockTestKubeClient::new();
+        mock_expect!(
                 client,
                 request,
                 [
@@ -373,12 +266,10 @@ mod tests {
                 ]
             );
 
-            let worker =
-                PodDescriptionWorker::new(&client, "default".to_string(), "test".to_string());
+        let worker = PodDescriptionWorker::new(&client, "default".to_string(), "test".to_string());
 
-            let result = worker.fetch().await;
+        let result = worker.fetch().await;
 
-            assert!(result.is_err());
-        }
+        assert!(result.is_err());
     }
 }
