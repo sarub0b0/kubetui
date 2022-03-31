18067779d5e1d7e403bcd85e82947ee17956e01b
kosay <ekr59uv25@gmail.com>
2022-04-01 01:32:31 +0900

    feat(description/svc): fetchしたserviceから必要な情報のみを抽出するtrait実装

--- src/event/kubernetes/network/description/service.rs
+++ src/event/kubernetes/network/description/service.rs
@@ -47,3 +47,126 @@ where
         Ok(value)
     }
 }
+mod extract {
+    use anyhow::Result;
+    use k8s_openapi::api::core::v1::Service;
+    use kube::api::ObjectMeta;
+
+    pub trait Extract {
+        fn extract(&self) -> Self
+        where
+            Self: Sized;
+    }
+
+    impl Extract for Service {
+        fn extract(&self) -> Self {
+            let annotations = if let Some(mut annotations) = self.metadata.annotations.clone() {
+                annotations.remove("kubectl.kubernetes.io/last-applied-configuration");
+                if annotations.is_empty() {
+                    None
+                } else {
+                    Some(annotations)
+                }
+            } else {
+                None
+            };
+            Service {
+                metadata: ObjectMeta {
+                    annotations,
+                    labels: self.metadata.labels.clone(),
+                    name: self.metadata.name.clone(),
+                    ..Default::default()
+                },
+                spec: self.spec.clone(),
+                status: self.status.clone(),
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use indoc::indoc;
+        use pretty_assertions::assert_eq;
+
+        use super::*;
+
+        fn service() -> Service {
+            serde_yaml::from_str(indoc! {
+                r#"
+                apiVersion: v1
+                kind: Service
+                metadata:
+                  annotations:
+                    kubectl.kubernetes.io/last-applied-configuration: |
+                      {"apiVersion":"v1","kind":"Service","metadata":{"annotations":{},"name":"service-0","namespace":"kubetui"},"spec":{"ports":[{"port":80,"targetPort":80}],"selector":{"app":"app"}}}
+                    foo: bar
+                  labels:
+                    foo: bar
+                  creationTimestamp: "2022-03-27T09:17:06Z"
+                  name: service-0
+                  namespace: kubetui
+                  resourceVersion: "714"
+                  uid: 7971c237-d5d8-468d-aeb9-ee6f9449c702
+                spec:
+                  clusterIP: 10.108.138.180
+                  clusterIPs:
+                  - 10.108.138.180
+                  internalTrafficPolicy: Cluster
+                  ipFamilies:
+                  - IPv4
+                  ipFamilyPolicy: SingleStack
+                  ports:
+                  - port: 80
+                    protocol: TCP
+                    targetPort: 80
+                  selector:
+                    app: app
+                  sessionAffinity: None
+                  type: ClusterIP
+                status:
+                  loadBalancer: {}
+                "#
+            })
+            .unwrap()
+        }
+
+        #[test]
+        fn 必要な情報のみを抽出してserviceを返す() {
+            let actual = service().extract();
+
+            let expected = serde_yaml::from_str(indoc! {
+                r#"
+                apiVersion: v1
+                kind: Service
+                metadata:
+                  annotations:
+                    foo: bar
+                  labels:
+                    foo: bar
+                  name: service-0
+                spec:
+                  clusterIP: 10.108.138.180
+                  clusterIPs:
+                  - 10.108.138.180
+                  internalTrafficPolicy: Cluster
+                  ipFamilies:
+                  - IPv4
+                  ipFamilyPolicy: SingleStack
+                  ports:
+                  - port: 80
+                    protocol: TCP
+                    targetPort: 80
+                  selector:
+                    app: app
+                  sessionAffinity: None
+                  type: ClusterIP
+                status:
+                  loadBalancer: {}
+                "#
+            })
+            .unwrap();
+
+            assert_eq!(actual, expected);
+        }
+    }
+}
