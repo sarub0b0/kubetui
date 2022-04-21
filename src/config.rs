c0b44896f0e1098cd587a6daafa908db54fe3cc9
kohashimoto <kohashimoto@bbtower.co.jp>
2022-04-21 15:11:13 +0900

    feat(config): --namespaces, --all-namespaces, --context引数を設定。引数の適用は未実装

--- src/config.rs
+++ src/config.rs
@@ -11,17 +11,52 @@ pub struct Config {
     #[clap(
         short,
         long,
-        default_value = "vertical",
-        possible_values = ["v", "h", "vertical", "horizontal"]
+        name = "v|h",
+        possible_values = ["v", "h", "vertical", "horizontal"],
+        display_order = 1000,
         )]
-    split_mode: DirectionWrapper,
+    split_mode: Option<DirectionWrapper>,
+
+    /// Namespaces
+    #[clap(
+        short,
+        long,
+        conflicts_with = "all-namespaces",
+        value_delimiter = ',',
+        display_order = 1000
+    )]
+    namespaces: Option<Vec<String>>,
+
+    /// Context
+    #[clap(short, long, display_order = 1000)]
+    context: Option<String>,
+
+    /// Select all namespaces
+    #[clap(
+        short = 'A',
+        long,
+        parse(try_from_str),
+        default_value_t = false,
+        value_name = "[true|false]",
+        possible_values = ["false", "true"],
+        min_values = 0,
+        require_equals = true,
+        default_missing_value = "true",
+        hide_default_value = true,
+        hide_possible_values = true,
+        display_order = 1000,
+        )]
+    all_namespaces: bool,
 }
 
 impl Config {
     pub fn split_mode(&self) -> Direction {
         match self.split_mode {
-            DirectionWrapper::Vertical => Direction::Vertical,
-            DirectionWrapper::Horizontal => Direction::Horizontal,
+            Some(d) => match d {
+                DirectionWrapper::Vertical => Direction::Vertical,
+                DirectionWrapper::Horizontal => Direction::Horizontal,
+            },
+            None => Direction::Vertical,
         }
     }
 }
@@ -52,4 +87,104 @@ impl FromStr for DirectionWrapper {
 
 pub fn configure() -> Config {
     Config::parse()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod split_mode {
+        use clap::ErrorKind;
+        use pretty_assertions::assert_eq;
+
+        use super::*;
+
+        #[test]
+        fn possible_valuesの値であるhを設定したときhorizontalを返す() {
+            let parse = Config::try_parse_from(["kubetui", "-s", "h"]).unwrap();
+            assert_eq!(parse.split_mode(), Direction::Horizontal)
+        }
+
+        #[test]
+        fn possible_valuesの値にない値を設定したときerrを返す() {
+            let parse = Config::try_parse_from(["kubetui", "-s", "hoge"]);
+            assert_eq!(parse.unwrap_err().kind(), ErrorKind::InvalidValue)
+        }
+    }
+
+    mod namespace {
+        use clap::ErrorKind;
+        use pretty_assertions::assert_eq;
+        use rstest::rstest;
+
+        use super::*;
+
+        #[test]
+        fn 値を設定しないとエラーを返す() {
+            let parse = Config::try_parse_from(["kubetui", "-n"]);
+            assert_eq!(parse.unwrap_err().kind(), ErrorKind::EmptyValue)
+        }
+
+        #[test]
+        fn namespaceを1つ指定したときvecを返す() {
+            let parse = Config::try_parse_from(["kubetui", "-n", "hoge"]).unwrap();
+            assert_eq!(parse.namespaces, Some(vec!["hoge".to_string()]))
+        }
+
+        #[rstest]
+        #[case::multiple_occurrences(&["kubetui", "-n", "foo", "-n", "bar", "-n", "zoo"])]
+        #[case::delimiter(&["kubetui", "-n", "foo,bar,zoo"])]
+        #[case::mixed(&["kubetui", "-n", "foo,bar", "-n", "zoo"])]
+        fn namespaceを複数指定したときvecを返す(#[case] iter: &[&str]) {
+            let parse = Config::try_parse_from(iter).unwrap();
+            assert_eq!(
+                parse.namespaces,
+                Some(vec![
+                    "foo".to_string(),
+                    "bar".to_string(),
+                    "zoo".to_string()
+                ])
+            )
+        }
+
+        #[test]
+        fn all_namespacesと併用するとエラーを返す() {
+            let parse = Config::try_parse_from(["kubetui", "-A", "-n", "hoge"]);
+            assert_eq!(parse.unwrap_err().kind(), ErrorKind::ArgumentConflict)
+        }
+    }
+    mod all_namespace {
+        use clap::ErrorKind;
+        use pretty_assertions::assert_eq;
+        use rstest::rstest;
+
+        use super::*;
+
+        #[test]
+        fn equalがない構文のときエラーになる() {
+            let parse = Config::try_parse_from(["kubetui", "--all-namespaces", "true"]);
+            assert_eq!(parse.unwrap_err().kind(), ErrorKind::UnknownArgument)
+        }
+
+        #[rstest]
+        #[case::is_true(true)]
+        #[case::is_false(false)]
+        fn 設定した値になる(#[case] value: bool) {
+            let parse = Config::try_parse_from(["kubetui", &format!("--all-namespaces={}", value)])
+                .unwrap();
+            assert_eq!(parse.all_namespaces, value)
+        }
+
+        #[test]
+        fn 値が設定されていないときtrueを設定する() {
+            let parse = Config::try_parse_from(["kubetui", "-A"]).unwrap();
+            assert_eq!(parse.all_namespaces, true)
+        }
+
+        #[test]
+        fn namespaceと併用するとエラーを返す() {
+            let parse = Config::try_parse_from(["kubetui", "-A", "-n", "hoge"]);
+            assert_eq!(parse.unwrap_err().kind(), ErrorKind::ArgumentConflict)
+        }
+    }
 }
