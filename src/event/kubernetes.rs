b2182441704991ce5aef2cadfbbabbd811643daf
kohashimoto <kohashimoto@bbtower.co.jp>
2022-04-27 01:13:04 +0900

    feat(kube_store): --all-namespacesや--contextなどに対応するためKubeStoreを実装

--- src/event/kubernetes.rs
+++ src/event/kubernetes.rs
@@ -246,7 +246,6 @@ async fn kube_worker_builder(
         };
 
         let config = Config::from_custom_kubeconfig(kubeconfig.clone(), &options).await?;
-
         let client = Client::try_from(config)?;
 
         let server_url = cluster_server_url(kubeconfig, named_context)?;
@@ -695,5 +694,217 @@ impl Worker for MainWorker {
         }
 
         Ok(WorkerResult::Terminated)
+    }
+}
+
+mod kube_store {
+    use std::{collections::HashMap, fmt::Debug};
+
+    use anyhow::Result;
+    use futures::future::try_join_all;
+    use kube::{
+        config::{KubeConfigOptions, Kubeconfig},
+        Client, Config,
+    };
+
+    use super::client::KubeClient;
+
+    pub type Context = String;
+
+    pub struct KubeState {
+        pub client: KubeClient,
+        pub namespaces: Vec<String>,
+        pub api_resources: Vec<String>,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub struct KubeStore {
+        inner: HashMap<Context, KubeState>,
+    }
+
+    impl From<HashMap<Context, KubeState>> for KubeStore {
+        fn from(inner: HashMap<Context, KubeState>) -> Self {
+            KubeStore { inner }
+        }
+    }
+
+    impl std::fmt::Debug for KubeState {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(
+                f,
+                "KubeStore {{ client: _, namespaces: {:?}, api_resources: {:?} }}",
+                self.namespaces, self.api_resources
+            )
+        }
+    }
+
+    impl PartialEq for KubeState {
+        fn eq(&self, rhs: &Self) -> bool {
+            self == rhs
+        }
+    }
+
+    impl KubeStore {
+        async fn try_from_kubeconfig(config: Kubeconfig) -> Result<Self> {
+            let Kubeconfig {
+                clusters,
+                contexts,
+                auth_infos,
+                ..
+            } = &config;
+
+            let jobs: Vec<(Context, KubeState)> =
+                try_join_all(contexts.iter().map(|context| async {
+                    let cluster = clusters.iter().find_map(|cluster| {
+                        if cluster.name == context.name {
+                            Some(cluster.name.to_string())
+                        } else {
+                            None
+                        }
+                    });
+
+                    let user = auth_infos.iter().find_map(|auth_info| {
+                        if auth_info.name == context.context.user {
+                            Some(auth_info.name.to_string())
+                        } else {
+                            None
+                        }
+                    });
+
+                    let options = KubeConfigOptions {
+                        context: Some(context.name.to_string()),
+                        cluster,
+                        user,
+                    };
+
+                    let config = Config::from_custom_kubeconfig(config.clone(), &options).await?;
+
+                    let cluster_url: String = config.cluster_url.to_string();
+                    let namespace = config.default_namespace.to_string();
+
+                    let client = Client::try_from(config)?;
+
+                    let kube_client = KubeClient::new(client, cluster_url);
+
+                    anyhow::Ok((
+                        context.name.to_string(),
+                        KubeState {
+                            client: kube_client,
+                            namespaces: vec![namespace],
+                            api_resources: vec![],
+                        },
+                    ))
+                }))
+                .await?;
+
+            let inner: HashMap<Context, KubeState> = jobs.into_iter().collect();
+
+            Ok(inner.into())
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        mod kube_state {
+            use indoc::indoc;
+            use pretty_assertions::assert_eq;
+
+            use super::*;
+
+            const CONFIG: &str = indoc! {
+                r#"
+                apiVersion: v1
+                clusters:
+                  - cluster:
+                      certificate-authority-data: ""
+                      server: https://192.168.0.1
+                    name: cluster-1
+                  - cluster:
+                      certificate-authority-data: ""
+                      server: https://192.168.0.2
+                    name: cluster-2
+                  - cluster:
+                      certificate-authority-data: ""
+                      server: https://192.168.0.3
+                    name: cluster-3
+                contexts:
+                  - context:
+                      cluster: cluster-1
+                      namespace: ns-1
+                      user: user-1
+                    name: cluster-1
+                  - context:
+                      cluster: cluster-2
+                      namespace: ns-2
+                      user: user-2
+                    name: cluster-2
+                  - context:
+                      cluster: cluster-3
+                      user: user-3
+                    name: cluster-3
+                current-context: cluster-2
+                kind: Config
+                preferences: {}
+                users:
+                  - name: user-1
+                    user:
+                      token: user-1
+                  - name: user-2
+                    user:
+                      token: user-2
+                  - name: user-3
+                    user:
+                      token: user-3
+                "#
+            };
+
+            #[tokio::test]
+            async fn kubeconfigからstateを生成() {
+                let kubeconfig = Kubeconfig::from_yaml(CONFIG).unwrap();
+
+                let actual = KubeStore::try_from_kubeconfig(kubeconfig).await.unwrap();
+
+                let expected = HashMap::from([
+                    (
+                        "cluster-1".to_string(),
+                        KubeState {
+                            client: KubeClient::new(
+                                Client::try_default().await.unwrap(),
+                                "https://192.168.0.1/",
+                            ),
+                            namespaces: vec!["ns-1".to_string()],
+                            api_resources: Default::default(),
+                        },
+                    ),
+                    (
+                        "cluster-2".to_string(),
+                        KubeState {
+                            client: KubeClient::new(
+                                Client::try_default().await.unwrap(),
+                                "https://192.168.0.2/",
+                            ),
+                            namespaces: vec!["ns-2".to_string()],
+                            api_resources: Default::default(),
+                        },
+                    ),
+                    (
+                        "cluster-3".to_string(),
+                        KubeState {
+                            client: KubeClient::new(
+                                Client::try_default().await.unwrap(),
+                                "https://192.168.0.3/",
+                            ),
+                            namespaces: vec!["default".to_string()],
+                            api_resources: Default::default(),
+                        },
+                    ),
+                ])
+                .into();
+
+                assert_eq!(actual, expected);
+            }
+        }
     }
 }
