eabb451c9572ee3bd4a96e07bade85b9681c5fd4
kosay <ekr59uv25@gmail.com>
2022-06-10 01:24:38 +0900

    feat(widget/input): text2で使用できるよう公開範囲を設定

--- src/tui_wrapper/widget/complex.rs
+++ src/tui_wrapper/widget/complex.rs
@@ -5,6 +5,7 @@ mod single_select;
 #[cfg(feature = "stack-widget")]
 mod stack;
 
+pub use input::InputForm;
 pub use multiple_select::{MultipleSelect, MultipleSelectBuilder};
 pub use single_select::{SingleSelect, SingleSelectBuilder};
 
