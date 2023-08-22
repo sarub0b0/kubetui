8900eeec8b7570c06dc3f277c842d3ff5ec32c16
kosay <ekr59uv25@gmail.com>
2023-08-23 00:32:09 +0900

    Revert "feat(ui): Keep the filter input field visible."

--- src/ui/widget/table/filter_form.rs
+++ src/ui/widget/table/filter_form.rs
@@ -39,10 +39,6 @@ impl FilterForm {
         self.input_widget.on_key_event(ev)
     }
 
-    pub fn clear(&mut self) {
-        self.input_widget.clear();
-    }
-
     pub fn render<B>(&mut self, f: &mut Frame<'_, B>, is_active: bool)
     where
         B: Backend,
