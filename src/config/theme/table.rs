b96f51f7ed7e8148ce6db546e9947f746e788e1b
kosay <ekr59uv25@gmail.com>
2024-11-02 13:53:34 +0900

    feat(theme): add theme support for select widget
    - Introduced `ListThemeConfig` in the theme configuration.
    - Updated `WidgetThemeConfig` to include `list` themes.
    - Modified `SingleNamespaceDialog` to accept and apply the new themes.
    - Added default implementations and builders for `FilterFormTheme` and
      `ListTheme`.
    - Updated tests to cover the new theme configurations.

--- src/config/theme/table.rs
+++ src/config/theme/table.rs
@@ -1,24 +1,9 @@
 use serde::{Deserialize, Serialize};
 
-use super::{BorderThemeConfig, ThemeStyleConfig};
+use super::FilterFormThemeConfig;
 
 #[derive(Default, Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct TableThemeConfig {
     #[serde(default)]
     pub filter: FilterFormThemeConfig,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
-pub struct FilterFormThemeConfig {
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub base: Option<ThemeStyleConfig>,
-
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub border: Option<BorderThemeConfig>,
-
-    #[serde(default)]
-    pub prefix: ThemeStyleConfig,
-
-    #[serde(default)]
-    pub query: ThemeStyleConfig,
 }
