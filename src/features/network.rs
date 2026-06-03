44cfb5752918e477b9c18884c5471ea985f38591
kohashimoto <kohashimoto@bbtower.co.jp>
2026-06-03 14:20:25 +0900

    feat(network): introduce NetworkColumn/NetworkColumnSpec/NetworkColumns types

--- src/features/network.rs
+++ src/features/network.rs
@@ -1,6 +1,8 @@
+mod columns;
 mod filter;
 pub mod kube;
 pub mod message;
 pub mod view;
 
+pub use columns::{NetworkColumn, NetworkColumnSpec, NetworkColumns, NetworkLabelColumn};
 pub use filter::network_filter_applicator;
